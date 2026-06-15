4c7f340a890a181733dae4a9800252e76cd1d4e3
Prashant Pareek <prashantpareek1988@gmail.com>
2026-06-15 18:18:16 +0530

    Added unit testing setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <section>
       <div className="max-w-7xl mx-auto px-4 h-10 flex mt-4 items-center">
+        <h1>Home</h1>
         <p>Hello There, Welcome to OpenCartX, an ecommerce web app build with next js</p>
       </div>
     </section>
